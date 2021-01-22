3e963adaf2b5ae5a3ca5495bceae7458ef90368b
“acaru” <alberto.canoruiz@gmail.com>
2021-01-22 18:08:16 +0100

    Añadir tareas, borrar tareas y guardarlas en BBDD

--- app/src/main/java/com/example/mistareasitt/LoginActivity.java
+++ app/src/main/java/com/example/mistareasitt/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.mistareasitt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +16,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+    }
+
+    public void crearCuenta(View view){
+        Toast toast = Toast.makeText(this,"Cuenta Creada", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void login(View view){
+        TextInputEditText usuario = (TextInputEditText) findViewById(R.id.cajaUser);
+        TextInputEditText pass = (TextInputEditText) findViewById(R.id.cajaPass);
+
+        if(usuario.getText().toString().equalsIgnoreCase("itt") && pass.getText().toString().equalsIgnoreCase("123")){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
--- app/src/main/java/com/example/mistareasitt/MainActivity.java
+++ app/src/main/java/com/example/mistareasitt/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.mistareasitt;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mistareasitt.db.ControladorDB;
 
 public class MainActivity extends AppCompatActivity {
+
+    ControladorDB controladorDB;
+    private ArrayAdapter<String> miAdapter;
+    ListView listViewTareas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controladorDB = new ControladorDB(this);
+        listViewTareas = (ListView) findViewById(R.id.listaTareas);
+        actualizarUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        final EditText cajaTexto = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Nueva tarea")
+                .setMessage("¿Qué quieres hacer a continuación?")
+                .setView(cajaTexto)
+                .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String tarea = cajaTexto.getText().toString();
+                        controladorDB.addTarea(tarea);
+                        actualizarUI();
+                    }
+                })
+                .setNegativeButton("Cancelar",null)
+                .create();
+        dialog.show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void actualizarUI(){
+        if(controladorDB.numeroRegistros()==0){
+            listViewTareas.setAdapter(null);
+        }else {
+            miAdapter = new ArrayAdapter<>(this, R.layout.item_tarea, R.id.task_title, controladorDB.obtenerTareas());
+            listViewTareas.setAdapter(miAdapter);
+        }
+    }
+
+    public void borrarTarea(View view){
+        View parent = (View) view.getParent();
+        TextView tareaTextView = (TextView) parent.findViewById(R.id.task_title);
+        String tarea = tareaTextView.getText().toString();
+        controladorDB.borrarTarea(tarea);
+        actualizarUI();
     }
 }
